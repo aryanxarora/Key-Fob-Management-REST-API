93f3681d5c5b58347cad9c4351c8e209149fc347
Jolson Cruz <56365930+jolsoncruz@users.noreply.github.com>
2023-12-10 20:55:42 -0500

    Added Controller Functionality

--- fob-api/src/main/java/ca/vanier/fobapi/controller/UserController.java
+++ fob-api/src/main/java/ca/vanier/fobapi/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.vanier.fobapi.services.UserService;
+import ca.vanier.systemlib.entity.User;
 
 @RestController
 @RequestMapping("/user")
@@ -19,59 +20,59 @@ public class UserController {
     private UserService us;
 
     @PostMapping("/save") // Create
-    public Response save(@RequestBody Response save) {
+    public Response save(@RequestBody User u) {
         Response res = new Response();
 
         try{
-            System.out.println("/USER/SAVE");
+            us.save(u);
+            res.setResult(u.toString() + " added.");
+            res.setStatus("201: Success");
         } catch (Exception e){
             res.setResult(e.toString());
-            res.setStatus("Fail");
-        }
-        
-        return res;
+            res.setStatus("500: Fail");
+        } return res;
     }
 
     @GetMapping("/find") // Read
-    public Response find(@RequestBody Response save) {
+    public Response find(@RequestBody Long id) {
         Response res = new Response();
 
         try{
-            System.out.println("/USER/FIND");
+            res.setResult(us.findById(id).get().toString() + " found.");
+            res.setStatus("200: Success");
         } catch (Exception e){
             res.setResult(e.toString());
-            res.setStatus("Fail");
-        }
-        
-        return res;
+            res.setStatus("500: Fail");
+        } return res;
     }
 
     @PutMapping("/update") // Update
-    public Response update(@RequestBody Response save) {
+    public Response update(@RequestBody User u) {
         Response res = new Response();
 
         try{
-            System.out.println("/USER/UPDATE");
+            User found = us.findById(u.getUserId()).get();
+            us.save(u);
+            res.setResult(found.toString() + " updated.");
+            res.setStatus("200: Success");
         } catch (Exception e){
             res.setResult(e.toString());
-            res.setStatus("Fail");
-        }
-        
-        return res;
+            res.setStatus("500: Fail");
+        } return res;
     }
 
     @DeleteMapping("/delete") // Delete
-    public Response delete(@RequestBody Response save) {
+    public Response delete(@RequestBody Long id) {
         Response res = new Response();
 
         try{
-            System.out.println("/USER/DELETE");
+            User found = us.findById(id).get();
+            res.setResult(found.toString() + " deleted.");
+            us.delete(found.getUserId());
+            res.setStatus("200: Success");
         } catch (Exception e){
             res.setResult(e.toString());
-            res.setStatus("Fail");
-        }
-        
-        return res;
+            res.setStatus("500: Fail");
+        } return res;
     }
-    
 }
